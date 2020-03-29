26e594e51e8a452a113f1dd91ec81d53e3ea6894
artur_fjodorov <artur.fodorov@gmail.com>
2020-03-29 18:49:34 +0300

    animation service + IntersectionObserver component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,101 +4,94 @@
       <BigHeading />
     </div>
     <div class="home-content">
-      <div class="home-item animate home-one is-red">
-        <h1 class="home-number">1</h1>
-        <div class="content">
-          <h2 class="is-white animate">Hello,</h2>
-          <h2 class="is-white animate">My name is</h2>
-          <h2 class="is-white animate">Artur Fedorov</h2>
+      <IntersectionObserver @enter="fadeIn">
+        <div class="home-item animate home-one is-red">
+          <h1 class="home-number">1{{msg}}</h1>
+          <div class="content">
+            <h2 class="is-white animate">Hello,</h2>
+            <h2 class="is-white animate">My name is</h2>
+            <h2 class="is-white animate">Artur Fedorov</h2>
+          </div>
         </div>
-      </div>
-      <div class="home-item animate home-two">
-        <h1 class="home-number">2</h1>
-        <div>
-          <h2 class="animate">I am</h2>
-          <h2 class="animate">
-            developer +
-            <span class="is-red">
+      </IntersectionObserver>
+      <IntersectionObserver @enter="fadeIn">
+        <div class="home-item animate home-two">
+          <h1 class="home-number">2</h1>
+          <div>
+            <h2 class="animate">I am</h2>
+            <h2 class="animate">
+              developer +
+              <span class="is-red">
               designer
             </span>
-          </h2>
-          <h2 class="animate">based in Saint-Petersburg</h2>
+            </h2>
+            <h2 class="animate">based in Saint-Petersburg</h2>
+          </div>
         </div>
-      </div>
-      <div class="home-item animate home-three">
-        <h1 class="home-number">3</h1>
-        <div>
-          <h2 class="animate">I am passionate about</h2>
-          <h2 class="animate">web development, design,</h2>
-          <h2 class="animate">grids, Swiss typography</h2>
-          <h2 class="animate">
-            and
-            <span class="is-red">
+      </IntersectionObserver>
+      <IntersectionObserver @enter="fadeIn">
+        <div class="home-item animate home-three">
+          <h1 class="home-number">3</h1>
+          <div>
+            <h2 class="animate">I am passionate about</h2>
+            <h2 class="animate">web development, design,</h2>
+            <h2 class="animate">grids, Swiss typography</h2>
+            <h2 class="animate">
+              and
+              <span class="is-red">
               Bauhaus
             </span>
-          </h2>
+            </h2>
+          </div>
         </div>
-      </div>
-      <div class="home-item animate home-four is-black">
-        <h1 class="home-number is-white">4</h1>
-        <div>
-          <h2 class="is-white animate">
-            development +
-            <span class="is-red">design</span>
-          </h2>
-          <h2 class="is-white animate">is my vision of</h2>
-          <h2 class="is-white animate">Bauhaus principle</h2>
-          <h2 class="home-text underlined is-white animate">
-            Forms follows Function
-          </h2>
+      </IntersectionObserver>
+      <IntersectionObserver @enter="fadeIn">
+        <div class="home-item animate home-four is-black">
+          <h1 class="home-number is-white">4</h1>
+          <div>
+            <h2 class="is-white animate">
+              development +
+              <span class="is-red">design</span>
+            </h2>
+            <h2 class="is-white animate">is my vision of</h2>
+            <h2 class="is-white animate">Bauhaus principle</h2>
+            <h2 class="home-text underlined is-white animate">
+              Forms follows Function
+            </h2>
+          </div>
         </div>
-      </div>
+      </IntersectionObserver>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import gsap, {Power1} from 'gsap';
+import {AnimationService} from '@/shared/services/animation.service';
 export default Vue.extend({
   name: 'Home',
   data() {
     return {
-      observer: {} as IntersectionObserver
+      observer: {} as IntersectionObserver,
+      msg: ''
     }
   },
   mounted(): void {
-    const config = { threshold: 0.5 }; //, root: document.querySelector('.main-content')
-    this.observer = new IntersectionObserver((entries, self) => {
-      const targets = entries.map(entry => {
-        if(entry.isIntersecting) {
-          self.unobserve(entry.target);
-          return entry.target;
-        }
-      });
-
-      // Call our animation function
-      this.fadeIn(targets);
-    }, config);
-
-    gsap.set('.animate', {
+    AnimationService.gsap.set('.animate', {
       duration: 1.5,
       opacity: 0,
       y: 50
     });
 
-    document.querySelectorAll('.animate').forEach(box => {
-      this.observer.observe(box);
-    });
   },
   methods: {
-    fadeIn(targets: (Element | undefined)[]) {
+    fadeIn() {
       // Without any offset
-      gsap.to(targets, {
+      AnimationService.gsap.to('.animate', {
         opacity: 1,
         y: 0,
-        stagger: 0.5,
-        ease: Power1.easeInOut
+        stagger: 0.2,
+        ease: AnimationService.easing.power1.easeInOut
       });
 
     }
