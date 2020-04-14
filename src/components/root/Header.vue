1e97e72f27783b2a2c5552c5a8e2f99b197d7891
artur_fjodorov <artur.fodorov@gmail.com>
2020-04-15 02:27:33 +0300

    navigation

--- src/components/root/Header.vue
+++ src/components/root/Header.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="header">
-    <a class="header-link">
+    <a
+      class="header-link"
+      @click="toggleNavigation">
       <SvgIcon
+        v-if="!showNavigation"
         class="header-icon"
         name="menu"
+        width="40"
+        height="40"/>
+      <SvgIcon
+        v-if="showNavigation"
+        class="header-icon-close"
+        name="close"
         width="40"
         height="40"/>
     </a>
@@ -11,22 +20,41 @@
 </template>
 <script lang="ts">
 import Vue from 'vue';
-import {routeWatcher} from '@/shared/mixins/route-watch.mixin';
+import {SET_SHOW_NAVIGATION, SHOW_NAVIGATION} from '@/store/ui';
 
 export default Vue.extend({
   name: 'Header',
-  mixins: [routeWatcher]
+  computed: {
+    showNavigation(): boolean {
+      return this.$store.getters[SHOW_NAVIGATION];
+    }
+  },
+  methods: {
+    toggleNavigation() {
+      this.$store.commit(SET_SHOW_NAVIGATION, !this.showNavigation);
+    }
+  }
 });
 </script>
 <style lang="scss" scoped>
   .header {
     display: flex;
-    height: 60px;
+    height: 80px;
     align-items: center;
     justify-content: flex-end;
+    position: relative;
+    z-index: 8;
+
+    &-link {
+      cursor: pointer;
+    }
 
     &-icon {
       stroke: $black;
+
+      &-close {
+        stroke: $white;
+      }
     }
   }
 </style>
---
+++ src/components/root/Navigation.vue
@@ -0,0 +1,208 @@
+<template>
+  <div class="navigation cover with-margin grid">
+    <div class="navigation-item is-reversed is-one">
+      <p class="p-italic navigation-item-caption is-white-text">01</p>
+      <router-link
+        to="/"
+        class="h2 navigation-item-text is-white-text">Home</router-link>
+    </div>
+
+    <div class="navigation-item is-two">
+      <div class="navigation-item-content">
+        <p class="p-italic navigation-item-caption is-white-text">02</p>
+        <a class="h2 navigation-item-text is-white-text">About Me</a>
+      </div>
+      <div class="navigation-item-secondary">
+        <p class="p-italic is-white-text">Short about me</p>
+      </div>
+    </div>
+
+    <div class="navigation-item is-three">
+      <div class="navigation-item-content">
+        <p class="p-italic navigation-item-caption is-white-text">03</p>
+        <a class="h2 navigation-item-text is-white-text">Projects</a>
+      </div>
+      <div class="navigation-item-secondary">
+        <p class="p-italic is-white-text">Development experience</p>
+        <p class="p-italic is-white-text">Design portfolio</p>
+      </div>
+    </div>
+
+    <div class="navigation-item is-four">
+      <div class="navigation-item-content">
+        <p class="p-italic navigation-item-caption is-white-text">04</p>
+        <a class="h2 navigation-item-text is-white-text">Contact</a>
+      </div>
+      <div class="navigation-item-secondary">
+        <p class="p-italic is-white-text">Let me know</p>
+      </div>
+    </div>
+
+    <div class="navigation-item is-bottom-left">
+      <div class="navigation-item-content">
+        <a class="h2 navigation-item-text is-white-text">Artur Fedorov</a>
+      </div>
+      <div class="navigation-item-secondary no-margin">
+        <p class="p-italic is-white-text">Developer |  Designer</p>
+      </div>
+    </div>
+
+    <ImageCover class="navigation-image"/>
+
+    <div class="navigation-item is-bottom-right">
+      <p class="p-italic is-white-text">
+        All rights reserved
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {AnimationService} from '@/shared/services/animation.service';
+export default Vue.extend({
+  name: 'Navigation',
+  mounted(): void {
+    const timeline = AnimationService.gsap.timeline({});
+
+    timeline.from('.navigation', {
+      duration: 0.5,
+      opacity: 0
+      // scaleY: 0.2
+    }).from('.navigation-item,  .navigation-image', {
+      duration: 1.3,
+      delay: -0.5,
+      y: -40,
+      x: 10,
+      opacity: 0,
+      scaleY: 0.8,
+      stagger: 0.1,
+      rotationX: 50,
+      rotationY: 20
+    })
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+  .navigation {
+    background-color: $black;
+    transform-origin: right top;
+
+    &-item {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      transform-origin: bottom right;
+
+      &-caption {
+        margin-right: 2em;
+      }
+
+      &-content {
+        display: flex;
+        align-items: flex-end;
+      }
+
+      &-secondary {
+        margin-top: 1em;
+        margin-left: 3.3em;
+        white-space: nowrap;
+
+        @media ($mobile) {
+          display: none;
+        }
+
+        &.no-margin {
+          margin-left: 0;
+        }
+      }
+
+      &-text {
+        line-height: 94%;
+        white-space: nowrap;
+      }
+
+      &.is-reversed {
+        flex-direction: row;
+        align-items: flex-end;
+      }
+
+      &.is-one {
+        grid-area: 2 / 1 / 3 / 1;
+      }
+
+      &.is-two {
+        grid-area: 3 / 9 / 4 / 10;
+
+        @media ($tablet) {
+          grid-area: 3 / 8 / 4 / 9;
+        }
+
+        @media ($mobile) {
+          grid-area: 3/ 1 / 4 / 1;
+        }
+      }
+
+      &.is-three {
+        grid-area: 4 / 4 / 5 / 5;
+
+        @media ($tablet) {
+          grid-area: 4 / 5 / 5 / 6;
+        }
+
+        @media ($mobile) {
+          grid-area: 4 / 1 / 5 / 1;
+        }
+      }
+
+      &.is-four {
+        grid-area: 5 / 5 / 6 / 6;
+
+        @media ($tablet) {
+          grid-area: 5 / 8 / 6 / 9;
+        }
+
+        @media ($mobile) {
+          grid-area: 5 / 1 / 7 / 1;
+        }
+      }
+
+      &.is-bottom-left {
+        grid-area: 7 / 1 / 8 / 2;
+
+        @media ($mobile) {
+          grid-area: 7 / 9 / 8 / 10;
+        }
+      }
+
+      &.is-bottom-right {
+        align-items: flex-end;
+        justify-content: flex-end;
+        grid-area: 7 / 9 / 8 / 10;
+
+        @media ($tablet) {
+          grid-area: 7 / 8 / 8 / 9;
+        }
+
+        @media ($mobile) {
+          grid-area: 7 / 9 / 8 / 10;
+        }
+      }
+    }
+
+    &-image {
+      grid-area: 4 / 1 / 6 / 2;
+
+      @media ($tablet) {
+        grid-area: 4 / 1 / 6 / 5;
+        z-index: -1;
+        min-width: 250px;
+      }
+
+      @media ($mobile) {
+        display: none;
+      }
+    }
+  }
+</style>
